be1ccb0365d5d8001d85d4766294cdedf73bd2f2
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-29 19:10:40 +0100

    Added the style to the work

--- index.php
+++ index.php
@@ -2,38 +2,63 @@
 <html lang="ita">
 
 <head>
+    <!-- META TAG + TITLE -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DISCHI JSON</title>
+
+
+    <!-- LINK STYLES + ICON -->
+    <link href="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" rel="icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!-- vue 3 -->
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <!-- axios -->
+    <link href="./styles/style.css" rel="stylesheet">
+
+
+    <!-- * SCRIPTS JS -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js" type="text/javascript" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js"
         integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        crossorigin="anonymous" referrerpolicy="no-referrer" type="text/javascript" defer></script>
+    <script src="js/script.js" type="text/javascript" defer></script>
 
 </head>
 
+
+
 <body>
     <div id="app">
-        <main>
-            <div class="container-fluid d-flex flex-wrap">
-                <article v-for="discInfos,key in discsInfos" :key='index' class='d-flex flex-column align-items-center'>
-                    <div class="my_img-container">
-                        <img :src="discInfos.poster" alt="" :title="discInfos.genre" class="w-100">
-                    </div>
-                    <h3>{{discInfos.title}}</h3>
-                    <span>{{discInfos.author}}</span>
-                    <span>{{discInfos.year}}</span>
-                </article>
+        <header class="py-3">
+            <div class="container-fluid px-5">
+                <a href='#' class="my_img-container ms-5">
+                    <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png"
+                        alt="logo" class="w-100 ms-4">
+                </a>
+            </div>
+        </header>
+
+
+
+        <main class="py-3 px-5">
+            <div class="container-fluid px-5">
+                <div class="my_cards-container d-flex flex-wrap px-5">
+                    <article v-for="discInfos,index in discsInfos" :key='index'
+                        class='d-flex flex-column align-items-center rounded rounded-3 p-4' :title="discInfos.genre">
+                        <div class=" my_img-container">
+                            <img :src="discInfos.poster" alt="" class="w-100">
+                        </div>
+
+                        <div class="my_card-info d-flex flex-column align-items-center pt-4 pb-2 text-center">
+                            <h3 class="my_title fw-bold">{{discInfos.title}}</h3>
+                            <h6>{{discInfos.author}}</h6>
+                            <h4 class="my_date fw-bold">{{discInfos.year}}</h4>
+                        </div>
+                    </article>
+                </div>
             </div>
         </main>
     </div>
-
-
-    <script src="js/script.js"></script>
 </body>
 
 </html>
